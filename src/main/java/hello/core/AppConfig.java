de74ab1b11965a98a93a98caf36f6e8ba0b1a126
starryram <boramplay@naver.com>
2023-03-26 17:57:42 +0900

    [feat] Spring container 사용을 위한 코드 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 전체를 관장하는 클래스를 하나 만들자.(전체 동작 방식을 구성하는 클래스)
+@Configuration // 스프링 전환을 위한 어노테이션 생성(애플리케이션의 설정 정보를 담당함 = Configuration)
 public class AppConfig {
     // 중복 코드 제거를 위해서 리팩터링 실행(윈도우: ctrl + alt + m)
     // 리팩터링 이유: 메서드명을 통해 어떤 일을 하는지 구체적으로 명확하게 보인다.
+    @Bean // spring 컨테이너에다가 등록시켜주기
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository()); // 생성자 주입
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
    //     return new FixDiscountPolicy(); 새로운 할인정책의 도입으로 비율 할인 적용
         return new RateDiscountPolicy();
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,23 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     //psvm치고 enter 누르면 바로 만들어짐
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
         //MemberService memberService = new MemberServiceImpl(); appConfig로부터 서비스 생성
+
+        // 관심사 분리
+        // AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
+
+        // spring으로의 전환
+        // Appconfig.class를 가져오므로써, 관련 설정정보를 다 가져온다.(@Bean 붙은걸 다 가져온다)
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member); // 회원가입 되어야지
 
