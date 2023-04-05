90c1dcd05c430aca294a549f2f3ecc69f357e1b0
starryram <boramplay@naver.com>
2023-04-05 16:13:45 +0900

    [feat] InitializingBean, DisposableBean 추가

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,11 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+// 이 둘의 인터페이스는 스프링 전용 인터페이스이다 보니, 코드가 스프링 전용 인터페이스에 의존하게 된다.
+// 초기화, 소멸 메서드의 이름을 변경할 수 없고, 외부 라이브러리에 적용이 불가하다.
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
     public NetworkClient(){
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url){
@@ -25,4 +28,17 @@ public class NetworkClient {
      public void disconnect(){
          System.out.println("close: " + url);
      }
+
+    @Override
+    public void afterPropertiesSet() throws Exception { // 프로퍼티 셋팅이 끝나면(의존관계 주입이 끝나면) 호출하겠다는 뜻.
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception { // 종료될 때 호출
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
 }
+
